10cbd4dc36ee2eb739850bcc198e745dbfc2190c
Alexander Kluth <clooze@pm.me>
2020-01-20 14:11:51 +0100

    Add install script for laziness
    Signed-off-by: Alexander Kluth <clooze@pm.me>

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WORKING_DIR=`pwd`
+cd ~
+
+# Install oh-my-tmux and custom config
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp $WORKING_DIR/tmux.conf.local ./.tmux.conf.local
+
+# Install vim-plug and custom config
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp $WORKING_DIR/vimrc ./.vimrc
+
