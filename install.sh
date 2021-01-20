cc9a02b5e8b4e812eb9d93bafd15e3a5333f5323
Alexander Kluth <alex@quackiquacki.org>
2021-01-20 09:24:16 +0100

    Install zsh-autosuggestions
    Signed-off-by: Alexander Kluth <alex@quackiquacki.org>

--- install.sh
+++ install.sh
@@ -37,6 +37,9 @@ install_and_setup_env() {
     # Install oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+    # Install zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
     # Install powerlevel10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
